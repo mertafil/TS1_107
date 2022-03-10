c23f15d7678c162534801bc1fd4c212b2df56b08
FilMer17 <58984349+FilMer17@users.noreply.github.com>
2022-03-10 15:02:18 +0100

    Add factorial method

--- src/main/java/cz/cvut/fel/ts1/App.java
+++ src/main/java/cz/cvut/fel/ts1/App.java
@@ -4,6 +4,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println(factorial(4));
+    }
+
+    public static int factorial(int num) {
+        int result = 1;
+        int i = 1;
+
+        while (i <= num) {
+            result *= i;
+            i++;
+        }
+
+        return result;
     }
 }
